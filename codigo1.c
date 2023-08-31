fea574d2f9aee14a14427daa8af82b092c5ac48e
Ricardo Takahashi <ricardotakahashi@usp.br>
2023-08-31 14:05:17 -0300

    add myfunction

--- codigo1.c
+++ codigo1.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
+#include "myfunction.h"
+
 int main(){																																																												
 	printf("SEL0456 - Trabalho\n");
 	printf("SELSELSELSELSELSELSELSELSELSELSELSELSELSEL\n");
 	double f = 9;
 	printf("piriri pororo: %g", sqrt(f));
+	
+	printf("teste do commit");
+	printf("%f", multiply(2,3));
 	
 	
 	return 0;
