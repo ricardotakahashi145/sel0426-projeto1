276f5e5ea0f69adceed6dbc141dd57e3dc3dde28
Ricardo Takahashi <ricardotakahashi@usp.br>
2023-08-31 13:57:21 -0300

    Makefile - script terminal

---
+++ Makefile
@@ -0,0 +1,2 @@
+codigo1: codigo1.c
+	gcc codigo1.c -o codigo1 -lm 
---
+++ codigo1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){																																																												
+	printf("SEL0456 - Trabalho\n");
+	printf("SELSELSELSELSELSELSELSELSELSELSELSELSELSEL\n");
+	double f = 9;
+	printf("piriri pororo: %g", sqrt(f));
+	
+	
+	return 0;
+}
